a8f817da2af2e7ca2f72a53da7377db140086289
Christian Cieza <ciezac15@gmail.com>
2016-07-02 12:42:25 -0500

    -Agregando documento de estructura de liberacion, -Agregando .bat para generacion de paquetes de liberacion

---
+++ Documentacion/CrearPaqueteDeLiberacion.bat
@@ -0,0 +1,9 @@
+set /p n_liberacion=Introduce el numero de Liberacion:
+cls
+md LIBERACION#%n_liberacion%_%date:/=-%
+cd LIBERACION#%n_liberacion%_%date:/=-%
+md Documentos
+md Fuentes
+md Imagenes
+md Librerias
+md Scripts
